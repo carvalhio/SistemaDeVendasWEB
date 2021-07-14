5c603d8ffe255b726634c68d24e87afdb1df9113
Rafael Carvalho <raffael.carvallho@gmail.com>
2021-07-14 15:16:30 -0300

    Primeiro teste de Controller e Razor

--- SistemaVendasWeb/Controllers/HomeController.cs
+++ SistemaVendasWeb/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SistemaVendasWeb.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Sistema de Vendas Web";
+           
 
             return View();
         }
