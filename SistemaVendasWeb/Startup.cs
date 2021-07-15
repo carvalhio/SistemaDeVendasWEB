684093db91b14ab740c7d0fbf8bccc9006093e2b
Rafael Carvalho <raffael.carvallho@gmail.com>
2021-07-14 23:39:26 -0300

    Aaptação MySQL e primeiro Migration

--- SistemaVendasWeb/Startup.cs
+++ SistemaVendasWeb/Startup.cs
@@ -37,7 +37,8 @@ namespace SistemaVendasWeb
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<SistemaVendasWebContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("SistemaVendasWebContext")));
+                    options.UseMySql(Configuration.GetConnectionString("SistemaVendasWebContext"), builder =>
+builder.MigrationsAssembly("SistemaVendasWeb")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
