8d81d8797291cc18c1c1d1f876e0e524e04ebccd
Rafael Carvalho <raffael.carvallho@gmail.com>
2021-07-14 22:14:55 -0300

    CRUD Scaffolding

---
+++ SistemaVendasWeb/Data/SistemaVendasWebContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SistemaVendasWeb.Models
+{
+    public class SistemaVendasWebContext : DbContext
+    {
+        public SistemaVendasWebContext (DbContextOptions<SistemaVendasWebContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SistemaVendasWeb.Models.Department> Department { get; set; }
+    }
+}
