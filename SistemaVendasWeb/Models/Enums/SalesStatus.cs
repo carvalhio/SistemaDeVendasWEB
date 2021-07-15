dc8c191b7e6e90154869b7fcdf5a9fe673391164
Rafael Carvalho <raffael.carvallho@gmail.com>
2021-07-15 12:57:32 -0300

    Outras Entidades e Segunda Migration

---
+++ SistemaVendasWeb/Models/Enums/SalesStatus.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace SistemaVendasWeb.Models.Enums
+{
+    public enum SalesStatus : int
+    {
+        Peding = 0,   //Pedente
+        Billed = 1,   //Faturado
+        Canceled = 2  //Cancelado
+    }
+}
