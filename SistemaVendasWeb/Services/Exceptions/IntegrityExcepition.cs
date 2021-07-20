37ab0cafeb1a3f0c231de3675db46b9fed106e29
Rafael Carvalho <raffael.carvallho@gmail.com>
2021-07-20 14:12:02 -0300

    Tratamento de exceção para Delete

---
+++ SistemaVendasWeb/Services/Exceptions/IntegrityExcepition.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaVendasWeb.Services.Exceptions
+{
+    public class IntegrityExcepition : ApplicationException
+    {
+        public IntegrityExcepition(string message) : base (message)
+        {
+
+        }
+    }
+}
