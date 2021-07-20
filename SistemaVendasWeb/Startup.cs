7c2337e17b6952c00a13db9cce3c7ba7ccd516c1
Rafael Carvalho <raffael.carvallho@gmail.com>
2021-07-20 10:01:12 -0300

    Localização e formato de números e datas

--- SistemaVendasWeb/Startup.cs
+++ SistemaVendasWeb/Startup.cs
@@ -2,12 +2,15 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using SistemaVendasWeb.Models;
 using SistemaVendas.Data;
 using SistemaVendasWeb.Services;
+using System.Globalization;
+using System.Collections.Generic;
 
 namespace SistemaVendasWeb
 {
@@ -45,6 +48,16 @@ builder.MigrationsAssembly("SistemaVendasWeb")));
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
+            var enUS = new CultureInfo("en-US");
+            var localizationOptions = new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture(enUS),
+                SupportedCultures = new List<CultureInfo> { enUS },
+                SupportedUICultures = new List<CultureInfo> { enUS }
+            };
+
+            app.UseRequestLocalization(localizationOptions);
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
