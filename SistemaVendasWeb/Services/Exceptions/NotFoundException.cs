724cfebed11b2e83a1e38233045cb5e70e11578a
Rafael Carvalho <raffael.carvallho@gmail.com>
2021-07-19 23:13:00 -0300

    Editar Vendedor e exceções de serviços personalizada

---
+++ SistemaVendasWeb/Services/Exceptions/NotFoundException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SistemaVendasWeb.Services.Exceptions
+{
+    public class NotFoundException : ApplicationException
+    {
+        public NotFoundException(string message) : base (message)
+        {
+
+        }
+    }
+}
